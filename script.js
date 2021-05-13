b955b050443c02db221b5ac7ad005dbcfe797f49
ImBatman-7 <sahuababhinav@gmail.com>
2021-05-13 22:28:31 +0530

    im out of fetch requests

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 // scipt here
-const client_id = 'appa';
-const count = 10;
+const imagecontainer = document.getElementById('image-container')
+const loader = document.getElementById('loader')
+
+let fetchedimages = []
+
+const apiKey = 'Z6bLigM8VMDedTgW-ZM1cdels9EnAAk8vp230K0u58c';
+const countnumber = 10;
 
 
-const apiURL = `https://api.unsplash.com/photos/random/client_id=${client_id}&count${count}`;
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${countnumber}`;
 
-async function scrollingimages(){
+async function apiFetch(){
     try{
         const response = await fetch(apiURL);
-        const data = response.json();
-        console.log(data)
+        fetchedimages = await response.json();
+        console.log(fetchedimages)
     }
     catch(error){
 
     }
+    renderimages();
 }
 
+function renderimages(){
+    fetchedimages.forEach(photo => {
+        // setting a tag
+        const anchortag = document.createElement('a');
+        anchortag.setAttribute('href', photo.links.html);
+        anchortag.setAttribute('target', '_blank');
+        
+        // setting img tag
+        const imgtag = document.createElement('img');
+        imgtag.setAttribute('src', photo.urls.regular);
+        imgtag.setAttribute('title', photo.alt_description);
+        imgtag.setAttribute('alt', photo.alt_description);
 
+        //arranging tags
+        anchortag.appendChild(imgtag);
+        imagecontainer.appendChild(anchortag);
+    });
+}
 // load
-scrollingimages();
+apiFetch();
