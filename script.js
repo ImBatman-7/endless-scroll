64fb269047831d995ffce79b175fa431449bc2a9
ImBatman-7 <sahuababhinav@gmail.com>
2021-05-14 22:06:35 +0530

    added infinite scroll algo

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const countnumber = 10;
 
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${countnumber}`;
 
+// for fetching data from the api server
 async function apiFetch(){
     try{
         const response = await fetch(apiURL);
@@ -22,23 +23,45 @@ async function apiFetch(){
     renderimages();
 }
 
-function renderimages(){
-    fetchedimages.forEach(photo => {
-        // setting a tag
-        const anchortag = document.createElement('a');
-        anchortag.setAttribute('href', photo.links.html);
-        anchortag.setAttribute('target', '_blank');
-        
-        // setting img tag
-        const imgtag = document.createElement('img');
-        imgtag.setAttribute('src', photo.urls.regular);
-        imgtag.setAttribute('title', photo.alt_description);
-        imgtag.setAttribute('alt', photo.alt_description);
 
-        //arranging tags
+function renderimages(){
+
+    // looping over fetched images
+    fetchedimages.forEach(photo => {
+
+        // creating a function which sets attributes by for looping in content dictionary
+        function setAttributes(tag, content){
+            for (key in content){
+                tag.setAttribute(key,content[key])
+            }
+        }
+        
+        // creating a tag and bashing attributes in it
+        const anchortag = document.createElement('a');
+        setAttributes(anchortag, {
+            'href':photo.links.html,
+            'target':'_blank'
+        })
+
+        const imgtag = document.createElement('img');
+        setAttributes(imgtag, {
+            'src': photo.urls.regular,
+            'title': photo.alt_description,
+            'alt': photo.alt_description
+        })
+        
+        // appending divs into divs
         anchortag.appendChild(imgtag);
         imagecontainer.appendChild(anchortag);
     });
 }
+
+//scrolling infinitely
+window.addEventListener('scroll', function(){
+    if (window.scrollY + window.innerHeight >= document.body.offsetHeight){
+        apiFetch();
+    }
+})
 // load
 apiFetch();
+
