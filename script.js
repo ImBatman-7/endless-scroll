76e3198abd98ae101211bd1b85d8e93f34fbcb05
ImBatman-7 <sahuababhinav@gmail.com>
2021-05-15 23:43:27 +0530

    loading again

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const imagecontainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 
-let fetchedimages = []
+let fetchedimages = [];
 
 const apiKey = 'Z6bLigM8VMDedTgW-ZM1cdels9EnAAk8vp230K0u58c';
 const countnumber = 10;
@@ -15,7 +15,6 @@ async function apiFetch(){
     try{
         const response = await fetch(apiURL);
         fetchedimages = await response.json();
-        console.log(fetchedimages)
     }
     catch(error){
 
@@ -23,9 +22,25 @@ async function apiFetch(){
     renderimages();
 }
 
+let reachedDown = false;
+let loadedimage = 0;
+let totalLoadedImages = 0;
+
+function imagesReachedBottom(){
+    //reachedDown;
+    console.log('img loaded')
+    loadedimage++;
+    if (loadedimage === totalLoadedImages){
+       reachedDown = true; 
+       console.log('reached down = ', reachedDown)
+    }       
+}
+
 
 function renderimages(){
-
+    totalLoadedImages = 0;
+    totalLoadedImages = fetchedimages.length;
+    console.log('total images loaded= ', totalLoadedImages)
     // looping over fetched images
     fetchedimages.forEach(photo => {
 
@@ -53,15 +68,19 @@ function renderimages(){
         // appending divs into divs
         anchortag.appendChild(imgtag);
         imagecontainer.appendChild(anchortag);
+
+        //when first image pack loads completely and reaches bottom hit load again
+        imgtag.addEventListener('load', imagesReachedBottom())
     });
 }
 
 //scrolling infinitely
 window.addEventListener('scroll', function(){
-    if (window.scrollY + window.innerHeight >= document.body.offsetHeight){
+    if (window.scrollY + window.innerHeight >= document.body.offsetHeight - 1000 && reachedDown){
+        reachedDown = false;
         apiFetch();
     }
 })
+
 // load
 apiFetch();
-
